34b58faf5d7f79ec5289e601cb66c0adc7f92813
Donatas Abraitis <donatas@opensourcerouting.org>
2025-07-29 16:20:46 +0300

    Update top.json
    Signed-off-by: Donatas Abraitis <donatas@opensourcerouting.org>

--- contrib/hyrox_top_averages.py
+++ contrib/hyrox_top_averages.py
@@ -172,8 +172,9 @@ def main() -> None:
     parser.add_argument("--out", default=None, help="Optional path to save JSON")
     args = parser.parse_args()
 
-    top_athletes_men = get_top_athletes(n=args.top, url=args.rankings_url)
-    top_athletes_women = get_top_athletes(n=args.top, url="https://www.hyresult.com/rankings/alltime/hyrox-women")
+    top = int(args.top / 2)
+    top_athletes_men = get_top_athletes(n=top, url=args.rankings_url)
+    top_athletes_women = get_top_athletes(n=top, url="https://www.hyresult.com/rankings/alltime/hyrox-women")
     top_athletes = top_athletes_men + top_athletes_women
 
     athletes_data: List[Dict[str, object]] = []
